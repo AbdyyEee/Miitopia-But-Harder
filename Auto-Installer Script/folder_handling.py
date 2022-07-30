71e31b5daab07e91c744cca7155d878d47049a81
Abdyy <82438230+AbdyyEee@users.noreply.github.com>
2022-07-31 00:47:43 +0400

    Even more code cleanup

--- Auto-Installer Script/folder_handling.py
+++ Auto-Installer Script/folder_handling.py
@@ -10,7 +10,7 @@ SWITCH_ID = "01003DA010E8A000"
 RYUJINX_SWITCH_ID = "01003da010e8a000"
 USA_ID = "00040000001B4E00"
 EU_ID = "00040000001B4F00"
-# JPN_ID = "0004000000178800" 
+# JPN_ID = "0004000000178800"
 
 # -- Download Links --
 LATEST_SWITCH_VERSION = "https://github.com/AbdyyEee/Miitopia-But-Harder/releases/download/Main/Miitopia.but.Harder.Switch.Ver.2.0.zip"
@@ -28,23 +28,24 @@ def download_and_move_3ds() -> None:
     with ZipFile("Miitopia.But.Harder.3DS.Ver.2.0.zip") as f:
         f.extractall()
     # USA
-    if isdir(fr"{mod_path_3ds}\{USA_ID}"):
-        rmtree(fr"{mod_path_3ds}\{USA_ID}\romfs", ignore_errors=True)
-        mkdir(fr"{mod_path_3ds}\{USA_ID}\romfs")
-        copytree("romfs", fr"{mod_path_3ds}\{USA_ID}\romfs", dirs_exist_ok=True)
+    if isdir(rf"{mod_path_3ds}\{USA_ID}"):
+        rmtree(rf"{mod_path_3ds}\{USA_ID}\romfs", ignore_errors=True)
+        mkdir(rf"{mod_path_3ds}\{USA_ID}\romfs")
+        copytree("romfs", rf"{mod_path_3ds}\{USA_ID}\romfs", dirs_exist_ok=True)
     # EU
-    if isdir(fr"{mod_path_3ds}\{EU_ID}"):
-        rmtree(fr"{mod_path_3ds}\{EU_ID}\romfs", ignore_errors=True)
-        mkdir(fr"{mod_path_3ds}\{EU_ID}\romfs")
-        copytree("romfs", fr"{mod_path_3ds}\{EU_ID}\romfs", dirs_exist_ok=True)
+    if isdir(rf"{mod_path_3ds}\{EU_ID}"):
+        rmtree(rf"{mod_path_3ds}\{EU_ID}\romfs", ignore_errors=True)
+        mkdir(rf"{mod_path_3ds}\{EU_ID}\romfs")
+        copytree("romfs", rf"{mod_path_3ds}\{EU_ID}\romfs", dirs_exist_ok=True)
+
 
 def download_and_move_switch(emulator: str) -> None:
     """Downloads the latest release of the Hard Mode mod on Github and automatically
     extracts and inserts the file into the Yuzu mod directory.
-    
+
     if the parameter "emulator" is set to "Ryujinx", it will move all the folders into
     the ryujinx mod paths, and vise versa, if it is set to Yuzu, it will do the same action.
-    """ 
+    """
     # Downloading
     download(LATEST_SWITCH_VERSION)
     yuzu_path = expanduser("~\AppData\Roaming\Yuzu\load")
@@ -52,18 +53,33 @@ def download_and_move_switch(emulator: str) -> None:
     # Unzipping
     with ZipFile("Miitopia.But.Harder.Switch.Ver.2.0.zip") as f:
         f.extractall()
-    
+
     match emulator:
         case "Yuzu":
-            if isdir(fr"{yuzu_path}\{SWITCH_ID}"):
-                rmtree(fr"{yuzu_path}\{SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version", ignore_errors=True)
-                mkdir(fr"{yuzu_path}\{SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version")
-                copytree("Miitopia But Harder Version 2.0 Final Release Switch Version", fr"{yuzu_path}\{SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version", dirs_exist_ok=True)
+            if isdir(rf"{yuzu_path}\{SWITCH_ID}"):
+                rmtree(
+                    rf"{yuzu_path}\{SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version",
+                    ignore_errors=True,
+                )
+                mkdir(
+                    rf"{yuzu_path}\{SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version"
+                )
+                copytree(
+                    "Miitopia But Harder Version 2.0 Final Release Switch Version",
+                    rf"{yuzu_path}\{SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version",
+                    dirs_exist_ok=True,
+                )
         case "Ryujinx":
-            if isdir(fr"{ryujinx_path}\{RYUJINX_SWITCH_ID}"):
-                rmtree(fr"{ryujinx_path}\{RYUJINX_SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version", ignore_errors=True)
-                mkdir(fr"{ryujinx_path}\{RYUJINX_SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version")
-                copytree("Miitopia But Harder Version 2.0 Final Release Switch Version", fr"{ryujinx_path}\{RYUJINX_SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version", dirs_exist_ok=True)
-
-
-        
+            if isdir(rf"{ryujinx_path}\{RYUJINX_SWITCH_ID}"):
+                rmtree(
+                    rf"{ryujinx_path}\{RYUJINX_SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version",
+                    ignore_errors=True,
+                )
+                mkdir(
+                    rf"{ryujinx_path}\{RYUJINX_SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version"
+                )
+                copytree(
+                    "Miitopia But Harder Version 2.0 Final Release Switch Version",
+                    rf"{ryujinx_path}\{RYUJINX_SWITCH_ID}\Miitopia But Harder Version 2.0 Final Release Switch Version",
+                    dirs_exist_ok=True,
+                )
